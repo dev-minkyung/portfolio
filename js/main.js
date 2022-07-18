6c8aa3fed2d60c397667f11d9fd6b5423735635d
dev-minkyung <minkyung.web@gmail.com>
2022-07-18 18:38:33 +0900

    Sticky Header 추가

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
+// Sticky Header
+const header = document.querySelector(".header");
+
+window.onscroll = function() {
+  if (window.pageYOffset > 0) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+};
+
+
+// footer 올해 연도 자동 입력
 const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); //올해의 연도가 입력됨.
+thisYear.textContent = new Date().getFullYear();
